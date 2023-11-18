24a68bf05063b2560753f4589d83a9515e943be6
Akshat Singh <akshatsingh@Akshats-MacBook-Pro.local>
2023-11-18 11:14:04 +0530

    setup

---
+++ src/components/IncompleteList.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { PrimaryInput } from './PrimaryInput';
+
+export const IncompleteList = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <View style={styles.container}>
+    <View style={styles.header}>
+      <TouchableOpacity onPress={toggleExpansion}>
+      <Text style={styles.heading}>Incomplete Tasks</Text>
+      </TouchableOpacity>
+    </View>
+      {isExpanded && (
+        <View>
+          <PrimaryInput dataName='task'/>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 1,
+      marginTop: 5,
+      backgroundColor: 'white',
+      width: '100%',
+      height: '50%',
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      backgroundColor: 'gray'
+    },
+    heading: {
+      fontSize: 18,
+      fontWeight: '600',
+      textTransform: 'capitalize',
+      color: 'black',
+      fontFamily: 'SpaceGrotesk-Regular',
+    },
+    seeAll: {
+      fontSize: 14,
+      textTransform: 'capitalize',
+      marginRight: 10,
+      color: 'black',
+      fontFamily: 'SpaceGrotesk-Regular',
+    },
+    services: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginTop: 20,
+      flexWrap: 'wrap',
+    },
+  });
---
+++ src/components/PrimaryInput.tsx
@@ -0,0 +1,117 @@
+import React, {useState,} from 'react';
+import {
+  View,
+  TextInput,
+  ViewStyle,
+  FlexStyle,
+  Button,
+} from 'react-native';
+import RNFS from 'react-native-fs';
+
+export type PrimaryInputProps = {
+  placeholder?: string;
+  width?: string | number;
+  maxWidth?: string;
+  maxLength?: number;
+  error?: boolean;
+  success?: boolean;
+  dataName: string;
+  disabled?: boolean;
+  fontSize?: number;
+  styleProps?: ViewStyle | FlexStyle;
+  prefillValue?: string;
+  onClick?: () => void;
+};
+export const PrimaryInput = (props: PrimaryInputProps) => {
+  const {
+    placeholder,
+    error,
+    success,
+    maxLength,
+    disabled = false,
+    fontSize = 16,
+    onClick,
+  } = props;
+
+  const [value, setValue] = useState('');
+  // const [endIcon, setEndIcon] = useState(faCircleXmark);
+  const [loading,] = useState(false);
+  const [inputVisible, setInputVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setInputVisible(true);
+  };
+
+  const handleSaveText = () => {
+    saveInputToFile(value);
+  };
+
+  const saveInputToFile = async (value: string) => {
+    try {
+        const directoryPath = `${RNFS.DocumentDirectoryPath}/outputs`;
+    
+        await RNFS.mkdir(directoryPath);
+    
+        const filename = 'tasks.txt';
+        const filePath = `${directoryPath}/${filename}`;
+    
+        await RNFS.writeFile(filePath, value, 'utf8');
+    
+        console.log(`User input saved to: ${filePath}`);
+        console.log(value);
+        
+      } catch (error) {
+        console.error('Error while saving user input:', error);
+      }
+  };
+
+  return (
+    <View>
+        {inputVisible ? (
+        <View
+            style={{
+            position: 'relative',
+            flexDirection: 'row',
+            alignItems: 'center',
+            }}>
+            <TextInput
+            style={{
+                marginTop: 6,
+                paddingLeft: 16,
+                paddingVertical: '4%',
+                marginBottom: 6,
+                fontFamily: 'SpaceGrotesk-Medium',
+                fontStyle: 'normal',
+                fontWeight: '500',
+                fontSize: fontSize,
+                lineHeight: 20,
+                width: '90%',
+                borderWidth: error ? 1 : success ? 1 : 2,
+                borderColor: error
+                ? 'red'
+                : success
+                ? 'green'
+                : 'white',
+                backgroundColor: error
+                ? '#FFEFED'
+                : success
+                ? '#E6F2ED'
+                : '#F6F6F6',
+            }}
+            onChangeText={setValue}
+            onTouchStart={onClick}
+            value={value}
+            keyboardType='default'
+            placeholder={placeholder}
+            multiline={true}
+            editable={!loading && !disabled}
+            maxLength={maxLength}
+            />
+            <Button title='done' onPress={handleSaveText}/>
+        </View>
+        ) : (
+        <Button title="Add task" onPress={handleButtonClick} />
+        )}
+    </View>
+  );
+};
